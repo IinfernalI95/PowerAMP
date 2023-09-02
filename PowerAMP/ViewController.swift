8ddd13c525e9dae5e4eb8bc76ce10cb3e94bc0ed
Артур Лебедев <IinfernalI95@gmail.com>
2023-09-02 20:27:37 +0300

    Initial Commit

---
+++ PowerAMP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PowerAMP
+//
+//  Created by Artur on 02.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
